0af760a10995ed72e5a19fa28d8d1365d825ea2c
ImSeokMin <imseokmin01@gmail.com>
2025-05-28 22:10:40 +0900

    wise_saying05

---
+++ src/main/java/com/back/App.java
@@ -0,0 +1,84 @@
+package com.back;
+
+import java.util.Scanner;
+
+public class App {
+    Scanner scanner = new Scanner(System.in);
+    int lastId = 0;
+    WiseSaying[] wiseSayings = new WiseSaying[100];
+    int wiseSayingsLastIndex = -1;
+
+    // 진입점 시작
+    void run() {
+        System.out.println("== 명언 앱 ==");
+
+        while (true){
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine().trim();
+
+            if (cmd.equals("종료")) {
+                break;
+            } else if (cmd.equals("목록")) {
+                actionList();
+            } else if(cmd.equals("등록")) {
+                actionWrite();
+            }
+        }
+        scanner.close();
+    }
+    //진입점 끝
+
+    // action(명령어) 시작
+    void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+
+        WiseSaying[] forListWiseSayings = findForList();
+
+        for(WiseSaying wiseSaying : forListWiseSayings){
+
+            System.out.printf("%d / %s / %s\n", wiseSaying.id, wiseSaying.content, wiseSaying.author);
+        }
+
+    }
+
+    void actionWrite() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine().trim();
+        System.out.print("작가 : ");
+        String author = scanner.nextLine().trim();
+
+        WiseSaying wiseSaying = write(content,author);
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n",wiseSaying.id);
+    }
+    // action(명령어) 끝
+
+    // 내부 로직 시작
+    int getSize() {
+        return wiseSayingsLastIndex + 1;
+    }
+
+    WiseSaying[] findForList() {
+        WiseSaying[] forListWiseSayings = new WiseSaying[getSize()];
+
+        int forListWiseSayingsIndex = -1;
+
+        for(int i = wiseSayingsLastIndex; i >= 0; i--) {
+            forListWiseSayings[++forListWiseSayingsIndex] = wiseSayings[i];
+        }
+
+        return forListWiseSayings;
+    }
+
+    WiseSaying write(String content, String author) {
+        WiseSaying wiseSaying = new WiseSaying();
+        wiseSaying.id = ++lastId;
+        wiseSaying.content = content;
+        wiseSaying.author = author;
+        wiseSayings[++wiseSayingsLastIndex] = wiseSaying;
+
+        return wiseSaying;
+    }
+    // 내부 로직 끝
+}
